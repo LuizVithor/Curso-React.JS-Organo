d6bf3f30fbc08d1d7b0a30ba82572537e55704b7
LuizVithor <luizmonodarius@gmail.com>
2023-01-02 19:56:32 -0300

    Fim do curso

---
+++ src/componentes/banner/index.js
@@ -0,0 +1,11 @@
+import banner from "./Banner.css";
+
+function Banner() {
+    return (
+        <header className="banner">
+        <img src="/imagens/banner.png" alt="Banner" />
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/componentes/rodape/index.js
@@ -0,0 +1,27 @@
+import "./rodape.css"
+
+const Rodape = () => {
+    return (
+            <div className="rodape2">
+                <section>
+                    <a href="https://www.linkedin.com/in/luizvithor/">
+                    <img src="/imagens/li.png"/>
+                    </a>
+                    <a href="https://github.com/LuizVithor">
+                    <img src="/imagens/git.png"/>
+                    </a>
+                </section>
+                
+                <div>
+                    <img src="/imagens/logo.png"/>
+                </div>
+                
+                <div>
+                    Desenvolvido por Luiz Vithor
+                </div>
+
+            </div>
+    )
+}
+
+export default Rodape
